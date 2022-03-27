400a3a2ad0b1ecbd68869bfd20e9578983eb7697
Akshata72 <sableakshata880@gmail.com>
2022-03-27 21:32:59 +0530

    Added:UC8-Insert_Node

--- Linked_List/UC8-InsertNode.cs
+++ Linked_List/UC8-InsertNode.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace LinkeDList
+
+namespace LinkdList
 {
     public class Node
     {
@@ -17,7 +18,7 @@ namespace LinkeDList
     }
     public class LinkedList
     {
-        
+
         public Node Head;
         public Node Tail;
         public LinkedList()
@@ -51,6 +52,23 @@ namespace LinkeDList
             }
             return false;
         }
+        public void InsertNode(int data)
+        {
+            Node node = new Node(data);
+            Node node_1 = new Node(70);
+            Node node_2 = new Node(30);
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Head.next = node_2;
+                Head.next.next = node;
+                Head.next.next.next = node_1;
+            }
+        }
         internal void Display()
         {
             Node temp = Head;
@@ -69,6 +87,36 @@ namespace LinkeDList
                 }
                 temp = temp.next;
             }
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Node node_3 = new Node(56);
+            Node node_2 = new Node(30);
+            Node node_1 = new Node(70);
+            linkedList.AddNode(node_1);
+            linkedList.AddNode(node_2);
+            linkedList.AddNode(node_3);
+            linkedList.Display();
+            Console.WriteLine("\nEnter number to Search");
+            int Value = int.Parse(Console.ReadLine());
+            if (linkedList.search(Value) != null)
+            {
+                Console.WriteLine("Node found");
+            }
+            else
+            {
+                Console.WriteLine("Node not found");
+            }
+            linkedList.Display();
+
+            Console.WriteLine("\nEnter a Value for Inserting after 30 :");
+            int node = int.Parse(Console.ReadLine());
+            linkedList.InsertNode(node);
+            linkedList.Display();
         }
     }
 }
